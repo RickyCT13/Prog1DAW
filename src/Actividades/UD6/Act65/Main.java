c33c7fbb4974932121c752bf74238c1205cf2b64
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-04-06 14:02:37 +0200

    Act 6.5 Terminada (Sin esquema DyV)

--- src/Actividades/UD6/Act65/Main.java
+++ src/Actividades/UD6/Act65/Main.java
@@ -3,7 +3,7 @@ package Actividades.UD6.Act65;
 public class Main {
     public static void main(String[] args) {
         int[] numbers  = {65, 41, 49, 84, 14, 91};
-        System.out.println(BinarySearch.search(numbers, 10));
+        System.out.println(BinarySearch.search(numbers, 14));
     }
 
 }
