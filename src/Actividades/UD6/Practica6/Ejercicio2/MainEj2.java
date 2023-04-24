3db4ca88596215bfcc6f6b51467d758525cd3fbb
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-04-24 11:09:22 +0200

    Act 7.2 realizada.

--- src/Actividades/UD6/Practica6/Ejercicio2/MainEj2.java
+++ src/Actividades/UD6/Practica6/Ejercicio2/MainEj2.java
@@ -3,7 +3,7 @@ package Actividades.UD6.Practica6.Ejercicio2;
 public class MainEj2 {
     public static void main(String[] args) {
         Rod sourceRod = new Rod(), destRod = new Rod(), auxRod = new Rod();
-        TowerOfHanoi.solve(5, sourceRod, destRod, auxRod);
+        TowerOfHanoi.solve(3, sourceRod, destRod, auxRod);
         System.out.println(sourceRod);
         System.out.println(auxRod);
         System.out.println(destRod);
--- src/Actividades/UD7/Act72/Main.java
+++ src/Actividades/UD7/Act72/Main.java
@@ -1,26 +1,71 @@
 package Actividades.UD7.Act72;
 
+import Actividades.UD6.Excepciones.ElementAlreadyInListException;
+
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
         // TODO: Crea un fichero llamado alumnos.txt dentro de una nueva carpeta con nombre 1DAW,
         //  controlando que si ya existe no haga nada. La carpeta debe estar en la raíz de tu usuario.
 
-
+        File alumnos = new File("src\\Actividades\\UD7\\Act72\\alumnos.txt");
+        if (!alumnos.createNewFile()) {
+            throw new FileAlreadyExistsException("El archivo ya existe");
+        }
 
         // TODO: Introduce los nombres de los alumnos de la clase, una alumno por línea del fichero.
         //  Utiliza un buffer para realizar esta operación
 
+        BufferedWriter bufWriter = new BufferedWriter(new FileWriter(alumnos));
+        String[] names = {"Fulanito Gómez", "Menganito Pérez", "Manolo el cabezón malévolo"};
+        for (String name : names) {
+            bufWriter.write(name + "\n");
+        }
+        bufWriter.flush();
 
-        // TODO:
+        // TODO: Copia (lee y escribe) el contenido del
+        //  anterior fichero a uno nuevo con nombre prog.txt
+
+        File prog = new File("src\\Actividades\\UD7\\Act72\\prog.txt");
+
+        copyFileContent(alumnos.getPath(), prog.getPath());
 
 
-        // TODO:
+        // TODO: Renombra el fichero a prog_codificado.txt
+
+        File progEnc = new File("src\\Actividades\\UD7\\Act72\\prog-codificado.txt");
+        copyFileContent(prog.getPath(), progEnc.getPath());
+        prog.delete();
 
 
-        // TODO:
+        // TODO: En este último fichero, añade al final un alumno nuevo
 
-
-        // TODO:
+        // Añadimos el parámetro append = true
+        FileWriter fileWriter = new FileWriter(progEnc.getPath(), true);
+        bufWriter = new BufferedWriter(fileWriter);
+        bufWriter.write("Un alumno nuevo");
+        bufWriter.close();
     }
 
+    private static void copyFileContent(String sourcePath, String destPath) throws IOException {
+        BufferedWriter bufWriter;
+        BufferedReader bufReader = new BufferedReader(new FileReader(sourcePath));
+        bufWriter = new BufferedWriter(new FileWriter(destPath));
+
+        String line;
+        while((line = bufReader.readLine()) != null) {
+            bufWriter.write(line + "\n");
+        }
+        bufWriter.close();
+        bufReader.close();
+    }
+    
+    // TODO: Sustituye el segundo y tercer caracter del nombre de cada alumno con una X
+
+    // (No he sabido cómo hacer esta última parte)
 }
